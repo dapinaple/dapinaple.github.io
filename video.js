98260879c386daf5e8c15eb269922cffd6b31786
Esteban Schmitt <estschmitt05@gmail.com>
2026-01-08 14:04:06 -0500

    Adding a player for yt video on site

--- video.js
+++ video.js
@@ -1,23 +1,32 @@
 const videoIds = [
   "dQw4w9WgXcQ",
-  "kXYiU_JCYtU",
-  "3tmd-ClpJxA",
-  "RgKAFK5djSk",
   "Zi_XLOBDo_Y",
-  "OPf0YbXqDm0"
+  "kXYiU_JCYtU"
 ];
 
 const grid = document.getElementById("videoGrid");
 
 videoIds.forEach(id => {
-  const video = document.createElement("a");
-  video.href = `https://www.youtube.com/watch?v=${id}`;
-  video.target = "_blank";
-  video.className = "video-card";
+  const wrapper = document.createElement("div");
+  wrapper.className = "video-card";
 
-  video.innerHTML = `
-    <img src="https://img.youtube.com/vi/${id}/hqdefault.jpg" alt="Guitar video thumbnail">
+  wrapper.innerHTML = `
+    <div class="video-thumb" data-id="${id}">
+      <img src="https://img.youtube.com/vi/${id}/hqdefault.jpg" alt="Guitar video">
+      <div class="play-button">▶</div>
+    </div>
   `;
 
-  grid.appendChild(video);
+  wrapper.querySelector(".video-thumb").addEventListener("click", () => {
+    wrapper.innerHTML = `
+      <iframe
+        src="https://www.youtube.com/embed/${id}?autoplay=1"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen>
+      </iframe>
+    `;
+  });
+
+  grid.appendChild(wrapper);
 });
